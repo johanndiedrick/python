c37b90ca479f0136124cb9579f0083541586d978
AK <kozovski.a@gmail.com>
2012-04-26 23:46:04 -0400

    Added error catching

--- search.py
+++ search.py
@@ -4,24 +4,23 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from random import choice
-
+# Take a variable, search google, find the lowest results, return one random, scrape and return
 
 										#function to use if we want to control size of string
-# from bs4 import SoupStrainer
-# 
-# only_a_tags = SoupStrainer("a")
-# 
-# only_tags_with_id_link2 = SoupStrainer(id="link2")
-# 
-# def is_short_string(string):
-#     return len(string) < 10
-# 
-# only_short_strings = SoupStrainer(text=is_short_string)
+										# from bs4 import SoupStrainer 
+										# only_a_tags = SoupStrainer("a")
+										# 
+										# only_tags_with_id_link2 = SoupStrainer(id="link2")
+										# 
+										# def is_short_string(string):
+										#     return len(string) < 10
+										# 
+										# only_short_strings = SoupStrainer(text=is_short_string)
 
 
-											# The request also includes the userip parameter which provides the end
-											# user's IP address. Doing so will help distinguish this legitimate
-											# server-side traffic from traffic which doesn't come from an end-user.
+										# The request also includes the userip parameter which provides the end
+										# user's IP address. Doing so will help distinguish this legitimate
+										# server-side traffic from traffic which doesn't come from an end-user.
 
 
 
@@ -38,7 +37,11 @@ results = results["responseData"]
 results = results['cursor']
 nofResults = (results["resultCount"])
 										# set a variable for search result pagination
-nom = long(nofResults.replace(",", ""))/ 10000
+nom = long(nofResults.replace(",", ""))/10000
+# if nom >= 100000:
+# 		nom = nom/10000
+# elif nom < 100000:
+# 		nom = nom/10000
 
 print "Results searched...." ,nom
 results = results["moreResultsUrl"]
@@ -74,20 +77,35 @@ for s in all:					#iterate through the document and extract links
                 				# list of lists
                 listOfUrls.append(sp[0])
 
-								#random url selected
-deathUrl = choice(listOfUrls)
-								#printing the selected url
-print "I am chosen to die!" + deathUrl
 
-								#open urls and parse 
-death= urllib2.urlopen(deathUrl)
-death = death.read()
-
-deathSoup = BeautifulSoup(death)
-print (forContext = (deathSoup.get_text()))
+def myloop(listOfUrls):
+	deathUrl = choice(listOfUrls)
+	listOfUrls.remove(deathUrl)
+	if len(listOfUrls) > 0: 
+		#printing the selected url
+		print "Dying! " + deathUrl
+		# for url in deathUrl:    #open urls and parse 
+		death= urllib2.urlopen(deathUrl)
+		try:
+			death = death.read()
+			deathSoup = BeautifulSoup(death)
+			#print (deathSoup.get_text())
+			for string in deathSoup.stripped_strings:
+				tocontextfree = repr(string)
+				print tocontextfree
+					
+					
+				
+		except (Exception,NameError,urllib2.HTTPError,urllib2.URLError):
+			# deathUrl = choice(listOfUrls)
+			print "I love errors"
+			print deathUrl
+			return myloop(listOfUrls)
+			
+print 'end'
+		
+myloop(listOfUrls)	
+		
+# print (deathSoup.get_text())
 							# a different HTML parse function using stripped string to remove white space
 
-							# for string in deathSoup.stripped_strings:
-							# 								#context free variable from site
-							#     tocontextfree = (string)
-							# print tocontextfree
